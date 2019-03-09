9f10b3c1aee82dfa51e6401cd61c1f7d8faba8b8
Xochil Squaglia <xochilsquaglia@yahoo.com>
2019-03-09 14:38:42 -0800

    setting up

---
+++ code-challenges/401/stacksAndQueues/stacks-and-queues.js
@@ -0,0 +1,25 @@
+'use strict'
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+
+
+class Stack (value) {
+    this.top = null;
+    this.next = value;
+}
+    push(value) {
+
+        return
+}
+    pop(value) {
+        return
+}
+    peek(value) {
+        return
+}
