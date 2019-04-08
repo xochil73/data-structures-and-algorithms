59ce76edd6029850f0b14b952c951d142365a940
Xochil Squaglia <xochilsquaglia@yahoo.com>
2019-04-08 12:00:29 -0700

    completed the code part

--- code-challenges/401/llMerge/ll_merge.js
+++ code-challenges/401/llMerge/ll_merge.js
@@ -3,8 +3,18 @@
 const Node = require('./node.js');
 const LinkedList = require('./linked_list.js');
 
+const linkedList1= new LinkedList();
+linkedList1.insertAtHead(5);
+linkedList1.insertAtHead(3);
+linkedList1.insertAtHead(1);
+const linkedList2= new LinkedList ();
+linkedList2.insertAtHead(6);
+linkedList2.insertAtHead(4);
+linkedList2.insertAtHead(2);
 
-  const  mergedLists =(a, b) => {
+
+
+const  mergedLists =(a, b) => {
 
         let currentListA = a.head;
         let currentListB = b.head;
@@ -12,28 +22,32 @@ const LinkedList = require('./linked_list.js');
         let tempB = currentListB.next;
 
 
-        while (currentListA && currentListB) {
+
+        while (tempA && tempB) {
+
+
+            currentListA.next = currentListB;
+            currentListB.next= tempA;
+            currentListA = tempA;
+            currentListB = tempB;
             tempA = currentListA.next;
             tempB = currentListB.next;
-            currentListA = currentListB;
-            currentListA = currentListA.next;
-            currentListA= tempB.next;
-            currentListB=currentListA.next;
-            currentListA=currentListA.next;
-
-
 
         }
+
+
+        return a;
+
   };
 
-  const linkedList1= new LinkedList();
-  linkedList1.insertAtHead(1);
-  linkedList1.insertAtHead(3);
-  linkedList1.insertAtHead(5);
-  const linkedList2= new LinkedList ();
-  linkedList2.insertAtHead(2);
-  linkedList2.insertAtHead(4);
-  linkedList2.insertAtHead(6);
+  // const linkedList1= new LinkedList();
+  // linkedList1.insertAtHead(5);
+  // linkedList1.insertAtHead(3);
+  // linkedList1.insertAtHead(1);cT
+  // const linkedList2= new LinkedList ();
+  // linkedList2.insertAtHead(6);
+  // linkedList2.insertAtHead(4);
+  // linkedList2.insertAtHead(2);
 
   mergedLists(linkedList1, linkedList2);
 
@@ -43,4 +57,5 @@ const LinkedList = require('./linked_list.js');
 
 
 
-  module.exports = mergedLists();
+
+  module.exports = mergedLists;
